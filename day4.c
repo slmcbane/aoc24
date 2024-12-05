75bb91c9939ee646404ceaf552cb5a4e5bce5903
Sean McBane <sean.mcbane@protonmail.com>
2024-12-04 21:14:04 -0600

    Complete day 4

--- day4.c
+++ day4.c
@@ -36,21 +36,47 @@ static bool search(str8 str, i32 i, i32 j, i32 rows, i32 cols, search_dir dir) {
   return true;
 }
 
-static i64 count_occurrences(str8 str, i32 rows, i32 cols) {
+typedef struct {
+  i64 xmas_count;
+  i64 x_mas_count;
+} counts;
+
+#define check_mas_down(str, i, j)                                              \
+  (str.data[IND(i + 1, j + 1)] == 'A' && str.data[IND(i + 2, j + 2)] == 'S')
+
+#define check_mas_up(str, i, j)                                                \
+  (str.data[IND(i - 1, j + 1)] == 'A' && str.data[IND(i - 2, j + 2)] == 'S')
+
+#define check_sam_down(str, i, j)                                              \
+  (str.data[IND(i + 1, j + 1)] == 'A' && str.data[IND(i + 2, j + 2)] == 'M')
+
+#define check_sam_up(str, i, j)                                                \
+  (str.data[IND(i - 1, j + 1)] == 'A' && str.data[IND(i - 2, j + 2)] == 'M')
+
+static counts count_occurrences(str8 str, i32 rows, i32 cols) {
   static const search_dir dirs[] = {{0, 1}, {0, -1}, {1, 0},  {-1, 0},
                                     {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
-  i64 count = 0;
+  i64 xmas_count = 0;
+  i64 x_mas_count = 0;
   for (i32 i = 0; i < rows; ++i) {
     for (i32 j = 0; j < cols; ++j) {
       if (str.data[IND(i, j)] == 'X') {
         for (unsigned k = 0; k < 8; ++k) {
-          count += search(str, i, j, rows, cols, dirs[k]);
+          xmas_count += search(str, i, j, rows, cols, dirs[k]);
+        }
+      } else if (i < rows - 2 && j < cols - 2) {
+        if ((str.data[IND(i, j)] == 'M' && check_mas_down(str, i, j)) ||
+            (str.data[IND(i, j)] == 'S' && check_sam_down(str, i, j))) {
+          if ((str.data[IND(i + 2, j)] == 'M' && check_mas_up(str, i + 2, j)) ||
+              (str.data[IND(i + 2, j)] == 'S' && check_sam_up(str, i + 2, j))) {
+            x_mas_count++;
+          }
         }
       }
     }
   }
 
-  return count;
+  return (counts){xmas_count, x_mas_count};
 }
 
 void day4(signal act, str8 line, arena *a, arena scratch) {
@@ -64,8 +90,9 @@ void day4(signal act, str8 line, arena *a, arena scratch) {
     builder = str8_builder_init(a, 1024);
     return;
   } else if (act == END_SIGNAL) {
-    i64 count = count_occurrences(builder.str, rows, cols);
-    printf("Day 4, Part 1: count = %ld\n", count);
+    counts result = count_occurrences(builder.str, rows, cols);
+    printf("Day 4, Part 1: count = %ld\n", result.xmas_count);
+    printf("Day 4, Part 2: count = %ld\n", result.x_mas_count);
     return;
   }
 
